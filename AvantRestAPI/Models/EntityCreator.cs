09dd760fb5615109eedfc946a9ae23d9d0c6c0b4
artem_gg <gmadrich@gmail.com>
2021-07-29 00:01:35 +0300

    DaData

--- AvantRestAPI/Models/EntityCreator.cs
+++ AvantRestAPI/Models/EntityCreator.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Dadata;
+using Dadata.Model;
 
 namespace AvantRestAPI.Models
 {
@@ -9,10 +11,11 @@ namespace AvantRestAPI.Models
     {
         private const string DaDataApiToken = "921e88969c6d8fa637fe7f5c3805e04c8ada1dff";
         private const string DaDataKey = "4468db5d2d1c6b5628fae74058f002371ea2766a";
+        private SuggestClientAsync _api = new SuggestClientAsync(DaDataApiToken, DaDataKey);
 
         public Contractor CreateContractor(string name, ContractorType type, string inn, string kpp = "")
         {
-            if (CheckContractorOnDaData(type, name, kpp))
+            if (CheckContractorOnDaData(type, name, kpp).Result)
                 return new Contractor()
                 {
                     Name = name,
@@ -24,8 +27,11 @@ namespace AvantRestAPI.Models
             throw new ArgumentException($"Contractor with inn = {inn} and kpp = {kpp} not found on DaData");
         }
 
-        protected bool CheckContractorOnDaData(ContractorType type, string inn, string kpp = "")
+        protected async Task<bool> CheckContractorOnDaData(ContractorType type, string inn, string kpp = "")
         {
+            var request = new FindPartyRequest(query: inn, kpp: kpp);
+            var response = await _api.FindParty(request);
+            var party = response.suggestions[0].data;
             return true;
         }
     }
